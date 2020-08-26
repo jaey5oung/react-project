7363c504dc5c20274a358e90be2d62f1a839eb69
장재영 <jangjaeyeong@MacBook-Pro-2.local>
2020-08-26 18:20:20 +0900

    도커합침

--- server/config/dev.js
+++ server/config/dev.js
@@ -1,4 +1,4 @@
 module.exports = {
   mongoURI:
-    "mongodb+srv://tiger:1234@cluster0-cgecu.mongodb.net/<dbname>?retryWrites=true&w=majority",
-};
+    "mongodb://tiger:5782@cluster0-shard-00-00.j0bsa.mongodb.net:27017,cluster0-shard-00-01.j0bsa.mongodb.net:27017,cluster0-shard-00-02.j0bsa.mongodb.net:27017/<dbname>?ssl=true&replicaSet=atlas-75a1qo-shard-0&authSource=admin&retryWrites=true&w=majority",
+}
